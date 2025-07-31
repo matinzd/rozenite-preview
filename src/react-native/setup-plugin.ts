ff083a5b7f5f80f50dbb69fa743028460859374f
matinzd <24797481+matinzd@users.noreply.github.com>
2025-07-31 15:43:06 +0200

    chore: remove unused code

--- src/react-native/setup-plugin.ts
+++ src/react-native/setup-plugin.ts
@@ -20,37 +20,7 @@ async function setupPlugin() {
   existingClient.onMessage("request-initial-data", () => {
     const previews = getPreviewComponents();
     existingClient.send("preview-list", previews);
-    console.log(`Sent initial preview list with entries`, previews);
   });
 }
 
-// function setupHMRListener() {
-//   if (!__DEV__) {
-//     return;
-//   }
-//   let isUpdating = false;
-//   DeviceEventEmitter.addListener("websocketMessage", (data) => {
-//     try {
-//       const response = JSON.parse(data.data);
-//       if (response.type === "update" && response.body?.modified?.length > 0) {
-//         isUpdating = true;
-//       }
-
-//       if (isUpdating && response?.type === "update-done") {
-//         isUpdating = false;
-//         // Do your changes here!
-//         console.log("HMR update done, reloading previews...");
-//         console.log("Detected removed components, updating registry...");
-//         import("./preview-registry").then(({ detectRemovedComponents }) => {
-//           detectRemovedComponents();
-//         });
-//       }
-//     } catch (e) {
-//       console.log("Failed to parse the webSocketMessage event data!", e);
-//     }
-//   });
-// }
-
 setupPlugin();
-
-// setupHMRListener();
