b8ca73f1ffd017e4e6fe9c794ff827a2bfb8873f
matinzd <24797481+matinzd@users.noreply.github.com>
2025-08-04 14:25:51 +0200

    fix(hmr): use dispose and add checks for babel plugin

--- src/react-native/preview-registry.ts
+++ src/react-native/preview-registry.ts
@@ -1,5 +1,5 @@
 import { ComponentType } from "react";
-import { Metadata, MetroModule, Preview } from "../shared/types";
+import { Metadata, Module, Preview } from "../shared/types";
 import { createSignalMap } from "../utils/signal-map";
 import { client } from "./setup-plugin";
 
@@ -17,7 +17,10 @@ export function getPreviewComponents(): Preview[] {
 }
 
 export function getComponentByName(name: string) {
-  return flattenRegistryEntries().find((entry) => entry.name === name)?.component || null;
+  return (
+    flattenRegistryEntries().find((entry) => entry.name === name)?.component ||
+    null
+  );
 }
 
 /**
@@ -25,7 +28,7 @@ export function getComponentByName(name: string) {
  * @internal
  */
 const __registerPreviewInternal = (
-  module: MetroModule,
+  module: Module,
   name: string,
   component: ComponentType,
   metadata?: Metadata
@@ -36,26 +39,23 @@ const __registerPreviewInternal = (
     module.id === undefined
   ) {
     console.warn(
-      `Cannot register preview "${name}" in production or in non Metro environment.`
+      `[Rozenite Preview Plugin] Cannot register preview "${name}" in production or in non Metro environment.`
     );
     return;
   }
 
   if (metadata?.isInsideReactComponent) {
     console.error(
-      'Do not call "registerPreview" inside a React lifecycle. Use it at the top level of your module.'
+      '[Rozenite Preview Plugin] Do not call "registerPreview" inside a React lifecycle. Use it at the top level of your module.'
     );
     return;
   }
 
   let moduleId = module.id;
 
-  const originalDisposeCallback = module.hot._disposeCallback;
-
-  module.hot._disposeCallback = () => {
-    originalDisposeCallback?.();
+  module.hot.dispose(() => {
     registry.delete(moduleId);
-  };
+  });
 
   const current = registry.get(module.id) || [];
 
@@ -73,8 +73,16 @@ const __registerPreviewInternal = (
  * @param name Preview name
  * @param component React component
  */
-export function registerPreview(name: string, component: React.ComponentType) {  
+export function registerPreview(name: string, component: React.ComponentType) {
+  if (arguments.length !== 4) {
+    console.warn("[Rozenite Preview Plugin] Babel plugin is not configured correctly. This will result in a broken preview.");
+    return;
+  }
+
   __registerPreviewInternal(
-    ...(arguments as unknown as [MetroModule, string, ComponentType, Metadata])
+    arguments[0] as Module,
+    arguments[1] as string,
+    arguments[2] as React.ComponentType,
+    arguments[3] as Metadata
   );
 }
